c5e3cec9971dd0239ee9ff694a4cdef4de538516
lukaku98 <alvarezlucas201@gmail.com>
2017-07-10 12:18:55 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,15 +1,32 @@
 function Mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
+	var numeros;
 	var respuesta="si";
+    var acumuladorneg=0;
+	var acumuladorpos=0;
+	var cantidadpos=0;
+	var cantidadneg=0;
 
-	while(respuesta!="no")
+	while(respuesta=="si"||respuesta=="SI")
 	{
-		
-	
+		numero=parseInt(prompt("ingrese un numero"));
+		while(isNaN(numero))
+		{
+			numero=parseInt(prompt("error,ingrese un numero"));
+		}
+		if(numero>0)
+		{
+			acumuladorneg= acumuladorneg + numero;
+		}
+		else if(numero<0)
+		{
+			acumuladorpos=acumuladorpos + numero
+		}
+		else
+		{
+			
+		}
 	}
 
 
